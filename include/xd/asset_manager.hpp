893aa41d302413a795574c8b1756cd529580fbec
Firas Assaad <assaad.firas@gmail.com>
2018-02-22 02:34:44 +0100

    Asset Manager - Remove Commented Out Code & Use unordered_map

--- include/xd/asset_manager.hpp
+++ include/xd/asset_manager.hpp
@@ -5,7 +5,6 @@
 #include <xd/asset_serializer.hpp>
 #include <boost/any.hpp>
 #include <unordered_map>
-#include <map>
 #include <cstddef>
 
 #ifdef BOOST_NO_VARIADIC_TEMPLATES
@@ -56,45 +55,11 @@ namespace xd
 			auto it = persistent_assets.find(cache_key);
 			if (it != persistent_assets.end())
 				return it->second;
-			// otherwise check if it exists in the non-persistent map
-			//auto& loaded_assets = get_asset_map<T>();
-			//auto it2 = loaded_assets.find(cache_key);
-			//if (it2 != loaded_assets.end()) {
-			//	// make sure it has not expired
-			//	if (auto handle = it2->second.lock()) {
-			//		// push it to the persistent asset map and remove from non-persistent map
-			//		persistent_assets.insert(std::make_pair(cache_key, handle));
-			//		loaded_assets.erase(it2);
-			//		return handle;
-			//	}
-			//	// it has expired, remove it
-			//	loaded_assets.erase(it2);
-			//}
 			// not loaded in either map, create it
 			typename T::ptr resource(new T(std::forward<Args>(args)...));
 			persistent_assets.insert(std::make_pair(cache_key, resource));
 			return resource;
 		}
-
-		//template <typename T, typename... Args>
-		//void release(Args&&... args)
-		//{
-		//	// create serializer for asset type and get cache key
-		//	asset_serializer<T> serializer;
-		//	auto cache_key = serializer(std::forward<Args>(args)...);
-
-		//	// check if it's in the persistent map
-		//	auto& persistent_assets = get_persistent_asset_map<T>();
-		//	auto it = persistent_assets.find(cache_key);
-		//	if (it != persistent_assets.end())
-		//		persistent_assets.erase(it);
-
-		//	// check if it's in the non-persistent map
-		//	auto& loaded_assets = get_asset_map<T>();
-		//	auto it2 = loaded_assets.find(cache_key);
-		//	if (it2 != loaded_assets.end())
-		//		loaded_assets.erase(it2);
-		//}
 #else
 		// generate overloads for load, load_persistent and release with file iteration (up to XD_MAX_ARITY parameters)
 		#define BOOST_PP_ITERATION_PARAMS_1 (3, (1, XD_MAX_ARITY, <xd/detail/iterate_asset_manager.hpp>))
@@ -114,24 +79,11 @@ namespace xd
 					break;
 				}
 			}
-
-			// get the non-persistent map
-			//auto& loaded_assets = get_asset_map<T>();
-			// iterate through each loaded asset
-			//for (auto it2 = loaded_assets.begin(); it2 != loaded_assets.end(); ++it2) {
-			//	// check whether to remove this resource
-			//	if (auto handle = it2->second.lock()) {
-			//		if (resource.get() == handle.get()) {
-			//			loaded_assets.erase(it2);
-			//			break;
-			//		}
-			//	}
-			//}
 		}
 
 	private:
-		std::map<std::size_t, boost::any> m_asset_type_map;
-		std::map<std::size_t, boost::any> m_persistent_asset_type_map;
+		std::unordered_map<std::size_t, boost::any> m_asset_type_map;
+		std::unordered_map<std::size_t, boost::any> m_persistent_asset_type_map;
 
 		template <typename T>
 		std::unordered_map<typename asset_serializer<T>::key_type, typename T::weak_ptr>& get_asset_map()
